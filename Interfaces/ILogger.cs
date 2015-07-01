919dbe59a0896d14ce35e4abed70f590ae331e29
Rasmus Jonasson <rasmus.jonasson91@gmail.com>
2015-07-01 20:37:31 +0200

    Backend General changes

--- Interfaces/ILogger.cs
+++ Interfaces/ILogger.cs
@@ -14,6 +14,6 @@ namespace BackendGeneral.Interfaces
 
         void LogUpdate<T>(T entity);
 
-        void Log(string loggingMessage);
+        void Log(string loggingMessage, string stackTrace);
     }
 }
